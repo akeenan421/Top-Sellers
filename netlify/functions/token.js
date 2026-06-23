cd4492a65705e7cba6191f361e5dbbf1876b1fbb
akeenan421 <alex@evanssneakers.com>
2026-06-23 08:14:30 -0400

    Update token.js

--- netlify/functions/token.js
+++ netlify/functions/token.js
@@ -1,1 +1,25 @@
-
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+  try {
+    const { shop, code, client_id, client_secret } = JSON.parse(event.body);
+    const response = await fetch(`https://${shop}/admin/oauth/access_token`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ client_id, client_secret, code })
+    });
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify(data)
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: e.message })
+    };
+  }
+};
